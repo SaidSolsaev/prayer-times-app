53335a862287b4482772b59901a9dd5c249de3f3
Said Solsaev <smsolsae@ulrik.uio.no>
2024-05-18 23:43:22 +0200

    update

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,14 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native'
+import {View, Text, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
+
+
 
 const AboutScreen = () => {
   return (
-    <View>
-      <Text>AboutScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>About Faithful Companion</Text>
+      <Text style={styles.slogan}>Your Daily Guide to Faith</Text>
+      
+      <Text style={styles.sectionTitle}>Purpose</Text>
+      <Text style={styles.text}>
+        Faithful Companion is designed to provide accurate prayer times for Muslims around the world, along with additional features to enhance your spiritual journey.
+      </Text>
+      
+      <Text style={styles.sectionTitle}>Features</Text>
+      <Text style={styles.text}>
+        - Accurate prayer times based on your location {"\n"}
+        - Notification reminders for each prayer{"\n"}
+        - Qibla direction to help you face Mecca{"\n"}
+        - Islamic calendar with important dates{"\n"}
+        - Customizable settings for prayer calculations{"\n"}
+        - Read the Holy Quran with translation and tafsir{"\n"}
+        - Learn and explore the 99 names of Allah with meanings and explanations
+      </Text>
+
+      <Text style={styles.sectionTitle}>Read the Quran</Text>
+      <Text style={styles.text}>
+        Access the complete text of the Holy Quran. Read Surahs with translations and tafsir to gain a deeper understanding of the messages and teachings of Islam.
+      </Text>
+
+      <Text style={styles.sectionTitle}>99 Names of Allah</Text>
+      <Text style={styles.text}>
+        Learn and memorize the 99 names of Allah (Asma-ul-Husna). Each name is accompanied by its meaning and a detailed explanation to help you appreciate the attributes of Allah.
+      </Text>
+      
+      <Text style={styles.sectionTitle}>Developers</Text>
+      <Text style={styles.text}>
+        This app was developed by Said Solsaev.
+      </Text>
+      
+      <Text style={styles.sectionTitle}>Contact</Text>
+      <Text style={styles.text}>
+        If you have any questions, feedback, or suggestions, please contact me at tooBe@Filled.com.
+      </Text>
+    </ScrollView>
   )
 }
 
 export default AboutScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+
+  slogan: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#555',
+  },
+
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+
+  text: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,199 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, Switch, StyleSheet, ScrollView, Button, Modal, FlatList, TouchableOpacity} from 'react-native';
+
 
 const SettingsScreen = () => {
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+  const [selectedCalculationMethod, setSelectedCalculationMethod] = useState("MWL");
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const [calculationMethodModalVisible, setCalculationMethodModalVisible] = useState(false);
+  const [languageModalVisible, setLanguageModalVisible] = useState(false);
+
+  const calculationMethods = [
+    { label: 'Muslim World League', value: 'MWL' },
+    { label: 'Umm al-Qura', value: 'UQ' },
+    { label: 'University of Islamic Sciences, Karachi', value: 'UISK' },
+  ];
+
+  const languages = [
+    { label: 'English', value: 'en' },
+    { label: 'Arabic', value: 'ar' },
+    { label: 'French', value: 'fr' },
+  ];
+
+  console.log(selectedCalculationMethod)
+
+  const MyModal = ({visible, onClose, data, setData}) => {
+    return (
+      <Modal
+        visible={visible}
+        transparent={true}
+        animationType='slide'
+        onRequestClose={() => onClose(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <FlatList
+              data={data}
+              keyExtractor={(item) => item.value}
+              renderItem={({item}) => (
+                <TouchableOpacity
+                  style={styles.modalItem}
+                  onPress={() => {
+                    setData(item.value)
+                    onClose(false)
+                  }}
+                >
+                  <Text style={styles.modalItemText}>{item.label}</Text>
+                </TouchableOpacity>
+              )}
+            />
+            <Button title="Lukk" onPress={() => onClose(false)}/>
+          </View>
+        </View>
+        
+      </Modal>
+    )
+  }
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
-  )
-}
+    <ScrollView >
+      <View style={styles.container}>
+        
+        <View style={styles.setting}>
+          <Text style={styles.sectionTitle}>Aktiver varslinger</Text>
+          <Switch
+            value={notificationsEnabled}
+            onValueChange={(value) => setNotificationsEnabled(value)}
+          />
+        </View>
+        
+          
+        <View style={styles.setting}>
+          <Text style={styles.sectionTitle}>Calculation Method</Text>
+          <Button title={selectedCalculationMethod} style={styles.button} onPress={() => setCalculationMethodModalVisible(true)}/>
+        </View>
 
-export default SettingsScreen
+        <View style={styles.setting}>
+          <Text style={styles.sectionTitle}>Language</Text>
+          <Button title={selectedLanguage} style={styles.button} onPress={() => setLanguageModalVisible(true)}/>
 
-const styles = StyleSheet.create({})
+        </View>
+
+        <View style={styles.modalContainer}>
+          <MyModal
+            visible={calculationMethodModalVisible}
+            setVisible={setCalculationMethodModalVisible}
+            onClose={setCalculationMethodModalVisible}
+            data={calculationMethods}
+            setData={setSelectedCalculationMethod}
+          />
+        </View>
+
+        <View style={styles.modalContainer}>
+          <MyModal
+            visible={languageModalVisible}
+            setVisible={setLanguageModalVisible}
+            onClose={setLanguageModalVisible}
+            data={languages}
+            setData={setSelectedLanguage}
+          />
+        </View>
+
+        <View style={styles.setting}>
+          <Text style={styles.sectionTitle}>Dark mode</Text>
+          <Switch
+            value={darkTheme}
+            onValueChange={(value) => setDarkTheme(value)}
+          />
+        </View>
+
+      </View>
+
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  
+  
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  setting: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+
+  button: {
+    padding: 10,
+    backgroundColor: '#ddd',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+  
+  modalContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+
+  modalView: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 25,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: 300,
+    height: 350
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+
+  modalItem: {
+    padding: 20,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    width: '100%',
+    alignItems: 'center',
+  },
+
+
+  modalItemText: {
+    fontSize: 18,
+  },
+  
+  
+});
+
+export default SettingsScreen;
